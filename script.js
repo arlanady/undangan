2bb11d0653b72a502d0c29916d6ae59b263d31b4
arlanady <75787310+arlanady@users.noreply.github.com>
2026-05-30 10:45:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function bukaUndangan() {
-  alert("Undangan dibuka 🎉");
+  const music = document.getElementById("music");
+
+  // play musik
+  music.play();
+
+  // optional: hilangkan cover / lanjut ke halaman berikut
+  document.querySelector(".overlay").style.display = "none";
+}
+function toggleMusic() {
+  const music = document.getElementById("music");
+
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
 }
